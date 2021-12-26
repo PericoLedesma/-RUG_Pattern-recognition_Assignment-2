d7d025f36b69fd9b644e282b09abe992f1e4a04b
rikvegter <38559198+rikvegter@users.noreply.github.com>
2021-12-26 12:20:02 +0100

    Created file that reads the data
    Stored the data in a .pkl file that can be loaded in another file very easily and very fast.

---
+++ Task 1_Images pipeline/read_data.py
@@ -0,0 +1,66 @@
+#This implementation is heavily based on
+#https://kapernikov.com/tutorial-image-classification-with-scikit-learn/
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pprint
+from collections import Counter
+pp = pprint.PrettyPrinter(indent = 4)
+import joblib
+from skimage.io import imread
+from skimage.transform import resize
+import os
+
+def read_data(src, pklname, include, width = 150, height = None):
+
+    height = height if height is not None else width
+
+    data = dict()
+    data['description'] = 'resized ({0}x{1})big cat images in rgb'.format(int(width), int(height))
+    data['label'] = []
+    data['filename'] = []
+    data['data'] = []
+
+    pklname = f"{pklname}_{width}x{height}px.pkl"
+
+    #Read all images in PATH, resize and write to DESTIONATION_PATH
+    for subdir in os.listdir(src):
+        if subdir in include:
+            print(subdir)
+            current_path = os.path.join(src, subdir)
+
+            for file in os.listdir(current_path):
+                if file[-3:] in {'jpg', 'png'}:
+                    im = imread(os.path.join(current_path, file))
+                    im = resize(im, (width, height))
+                    data['label'].append(subdir[:-4])
+                    data['filename'].append(file)
+                    data['data'].append(im)
+        joblib.dump(data, pklname)
+
+def print_summary(data):
+    print('number of samples: ', len(data['data']))
+    print('labels', list(data.keys()))
+    print('description: ', data['description'])
+
+    Counter(data['label'])
+
+def main():
+    data_path = 'data/BigCats/' #Specify data path
+    classes = os.listdir(data_path) #Define classes
+    if '.DS_Store' in classes: classes.remove('.DS_Store') #For Mac Users, unwanted folder
+
+    base_name = 'big_cats'
+    width = 80
+    include = {'Leopard', 'Tiger', 'Cheetah', 'Jaguar', 'Lion'} #Include the wanted classes
+
+    #Read the data
+    read_data(src = data_path, pklname = base_name, width = width, include = include)
+    data = joblib.load(f'{base_name}_{width}x{width}px.pkl')
+
+    print_summary(data)
+
+
+
+if __name__ == "__main__":
+    main()
